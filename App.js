809d1746be4851fc129bbb66181d4a4a001226d0
Sandro Gomes <sandrogomes101@yahoo.com>
2023-01-07 16:27:12 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, ToastAndroid } from 'react-native';
+import VirtualKeyboard from 'react-native-virtual-keyboard';
+import { useState } from 'react';
 
 export default function App() {
+
+  const secret = '123456';
+  const [password, setPassword] = useState();
+  const [count, setCount] = useState();
+
+  const wrongPassword = () => {
+    ToastAndroid.showWithGravity(
+      "Wrong password",
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM
+    );
+  };
+
+  const correctPassword = () => {
+    ToastAndroid.showWithGravity(
+      "Correct password",
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM
+    );
+  };
+
+  function enterPassword(value) {
+    if (value == 'back') {
+      setPassword();
+      setCount();
+    } else {
+      if (typeof (password) == 'undefined') {
+        setPassword(value)
+      } else {
+        setPassword(password + value)
+      }
+      if (typeof (count) == 'undefined') {
+        setCount(2);
+      } else {
+        setCount(count + 1);
+      }
+      if (count === 6) {
+        setPassword();
+        setCount();
+        checkPassword(password + value);
+      }
+    }
+  }
+
+  function checkPassword(password) {
+    if (password === secret) {
+      correctPassword();
+    } else {
+      wrongPassword();
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text style={styles.title}>Insira sua senha</Text>
+        <TextInput secureTextEntry={true} value={password} style={styles.input} editable={false} ></TextInput>
+        <VirtualKeyboard style={styles.keyboard} color='black' applyBackspaceTint={false} textStyle={{ paddingTop: 10, paddingBottom: 10 }} pressMode='char' onPress={(value) => enterPassword(value)} />
+      </View>
+    </>
   );
 }
 
@@ -15,6 +72,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  input: {
+    width: '60%',
+    textAlign: 'center',
+    fontSize: 32,
+    color: 'black',
+  },
+  title: {
+    fontFamily: 'Roboto',
+    fontSize: 26,
+    top: 40,
+  },
+  keyboard: {
+    bottom: 20,
+  }
 });
